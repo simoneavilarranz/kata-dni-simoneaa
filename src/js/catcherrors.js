86970a9778627998068657553db702f0251c9e10
unknown <avilarranz@gmail.com>
2026-06-25 09:18:34 +0200

    feat:refactor number validation

--- src/js/catcherrors.js
+++ src/js/catcherrors.js
@@ -1,35 +1,26 @@
-const numbInput = document.getElementById('number-form');
-const errorMessage = document.getElementById('error-message');
-const calculateButton = document.getElementById('calculate-button');
+const numbInput = document.getElementById("number-form");
+const errorMessage = document.getElementById("error-message");
+const calculateButton = document.getElementById("calculate-button");
 /*const passwordInput = document.getElementById('login-password');
 const rememberCheckbox = document.getElementById('login-remember');
 const loginBtn = document.getElementById('login-btn');*/
 
-function showError(message) {
-    errorMessage.textContent = message;
-    errorMessage.style.display = 'block';
+export function showError(message) {
+  errorMessage.textContent = message;
+  errorMessage.style.display = "block";
 }
 
-function hideError() {
-    errorMessage.textContent = '';
-    errorMessage.style.display = 'none';
+export function hideError() {
+  errorMessage.textContent = "";
+  errorMessage.style.display = "none";
 }
 
-calculateButton.addEventListener('click', async function() {
-    hideError();
-    
-    const invalidNumb = Number.length != 8;
-    const invalidString = !Number;
-    
-    if (invalidNumb) {
-        showError('Introduce un número de 8 dígitos');
-        numbInput.focus();
-        return;
-    }
-    
-    if (invalidString) {
-        showError('Introduce un número valido');
-        numbInput.focus();
-        return;
-    }
-})
+calculateButton.addEventListener("click", async function () {
+  hideError();
+
+  if (!/^\d{8}$/.test(numbInput.value)) {
+    showError("Introduce un número de 8 dígitos");
+    numbInput.focus();
+    return;
+  }
+});
